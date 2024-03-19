d8a79654942291d92a5fa1c517789dc56387e8f8
s26822 <s26822@pjwstk.edu.pl>
2024-03-19 01:56:03 +0100

    Added avg method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello");
+static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+    {
+        throw new ArgumentException("Input array cannot be empty or null.");
+    }
+
+    int sum = 0;
+    foreach (int num in numbers)
+    {
+        sum += num;
+    }
+
+    return (double)sum / numbers.Length;
+}
